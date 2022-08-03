abb2628851cdb3d5b28e249499d68290397d2e1c
xshady <fastik036@gmail.com>
2022-08-03 17:57:03 +0300

    fix npm packages with `@`/`.` in name

--- src/plugin/shared/util/var-name.ts
+++ src/plugin/shared/util/var-name.ts
@@ -1,7 +1,7 @@
 export const codeVarName = (name: string): string => {
   return (
     "___altvEsbuild_" +
-    (name.replace(/[-/\\ ]/g, "_")) +
+    (name.replace(/[-/\\ @.]/g, "_x_")) +
     "___"
   )
 }
