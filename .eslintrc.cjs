a490068cb2bad3ebe49c6ab7418f31ddf4887b96
xshady <fastik036@gmail.com>
2022-12-13 03:26:56 +0300

    change eslint stuff

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,7 +14,7 @@ module.exports = {
     extraFileExtensions: ['.cjs']
   },
   rules: {
-    'no-new': 0,
+    'no-new': 'off',
     semi: ['warn', 'never'],
     'require-await': 'error',
     quotes: ['warn', 'double'],
@@ -32,11 +32,14 @@ module.exports = {
     '@typescript-eslint/explicit-member-accessibility': ['error', {
       ignoredMethodNames: ['constructor'],
     }],
-    'eol-last': 0,
-    '@typescript-eslint/no-empty-function': 0,
+    'eol-last': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/consistent-type-imports': 'error',
     '@typescript-eslint/explicit-function-return-type': ['error', {
+      allowHigherOrderFunctions: true,
+      allowConciseArrowFunctionExpressionsStartingWithVoid: true,
     }],
+    'no-void': 'off',
     'comma-dangle': 'off',
     '@typescript-eslint/comma-dangle': ['warn', {
       arrays: 'always-multiline',
@@ -66,7 +69,7 @@ module.exports = {
       'ts-check': false,
       minimumDescriptionLength: 3,
     }],
-    'node/handle-callback-err': 0,
+    'node/handle-callback-err': 'off',
     '@typescript-eslint/type-annotation-spacing': ['error', {
       'before': false,
       'after': true,
@@ -78,41 +81,37 @@ module.exports = {
       }
     }],
     '@typescript-eslint/await-thenable': 'error',
-    'func-call-spacing': 0,
-    '@typescript-eslint/func-call-spacing': 0,
-    'indent': 0,
+    'func-call-spacing': 'off',
+    '@typescript-eslint/func-call-spacing': 'off',
+    'indent': 'off',
     '@typescript-eslint/indent': ['error', 2],
-    'no-useless-escape': 0,
-    'space-infix-ops': 0,
+    'no-useless-escape': 'off',
+    'space-infix-ops': 'off',
     '@typescript-eslint/space-infix-ops': ['error'],
-    semi: 0,
+    semi: 'off',
     '@typescript-eslint/semi': ['warn', 'never'],
-    'lines-between-class-members': 0,
+    'lines-between-class-members': 'off',
     '@typescript-eslint/lines-between-class-members': ['error',  'always', {
       'exceptAfterSingleLine': true
     }],
-    'no-use-before-define': 0,
+    'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': ['error', {
       'functions': false, 
       'classes': true, 
       'variables': true
     }],
-    'space-before-function-paren': ['error', {
-      anonymous: 'never',
-      named: 'never',
-      asyncArrow: 'always',
-    }],
     'curly': ['error', 'multi-or-nest'],
-    'import/no-webpack-loader-syntax': 0,
-    'space-before-function-paren': ['error', {
+    'import/no-webpack-loader-syntax': 'off',
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/space-before-function-paren': ['error', {
       anonymous: 'never',
       named: 'never',
       asyncArrow: 'always',
     }],
     'brace-style': ['error', 'stroustrup'],
-    'dot-notation': 0,
+    'dot-notation': 'off',
     '@typescript-eslint/dot-notation': ['error'],
-    'no-multi-str': 0,
+    'no-multi-str': 'off',
   },
   ignorePatterns: [
     '.eslintrc.cjs',
