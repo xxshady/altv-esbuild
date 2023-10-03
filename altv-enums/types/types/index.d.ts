aec11f6d055a6d6cdba62cdd52dbda4a403b619a
xshady <fastik036@gmail.com>
2023-10-03 16:34:17 +0300

    update enums

--- altv-enums/types/types/index.d.ts
+++ altv-enums/types/types/index.d.ts
@@ -4,7 +4,7 @@
 /// <reference types="@altv/types-shared"/>
 declare module "altv-enums" {
   export { BaseObjectType, BlipSprite, BlipColor, BlipType, MarkerType, ColShapeType, AudioCategories, FileEncoding, Permission, CheckpointType, ModelType, KeyCode, BodyPart, PedConfigFlag, VoiceConnectionState, VehicleLockState, } from "alt-shared";
-  export { ExplosionType, RadioStation, NumberPlateStyle, VehicleBumper, VehicleBumperDamage, VehicleDoor, VehicleDoorState, VehicleModType, VehiclePart, VehiclePartDamage, WeatherType, WindowTint, ConnectDeniedReason, AmmoSpecialType, } from "alt-server";
+  export { ExplosionType, RadioStation, NumberPlateStyle, VehicleBumper, VehicleBumperDamage, VehicleDoor, VehicleDoorState, VehicleModType, VehiclePart, VehiclePartDamage, WeatherType, WindowTint, BaseObjectFilterType, ConnectDeniedReason, AmmoSpecialType, } from "alt-server";
   export { Locale, PermissionState, StatName, VehicleIndicatorLights, WebSocketReadyState, ConfigFlag, WatermarkPosition, GameFont, TextAlign, } from "alt-client";
   
 }
