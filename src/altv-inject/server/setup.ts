e046bc232fd16c8054413668271796ec7d92167a
xshady <fastik036@gmail.com>
2024-06-25 15:01:34 +0300

    forgot again

--- src/altv-inject/server/setup.ts
+++ src/altv-inject/server/setup.ts
@@ -18,7 +18,7 @@ import {
 } from "../shared"
 import type net from "net"
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
-import alt from "alt-server"
+import type alt from "alt-server"
 import { ControlledPromise, SocketConnect } from "@/shared/util"
 import { INSTANCE_ID_META_KEY, RESOURCE_RESTARTED_META_KEY } from "./constants"
 import type path from "path"
@@ -325,7 +325,7 @@ export class ServerSetup {
 
     this.log.info(`start a timer for ~cl~${playersReconnectDelay}~w~ ms to reconnect players (${players.length})`)
 
-    const spawnAfterConnect = alt.getServerConfig()["spawnAfterConnect"] as boolean ?? false
+    const spawnAfterConnect = _alt.getServerConfig()["spawnAfterConnect"] as boolean ?? false
 
     setTimeout(() => {
       for (const p of players) {
